ef2425d70de6d3369c9dbef663d42bf52819020e
Chibisov Gennady <web-chib@yandex-team.ru>
2014-07-28 20:15:57 +0400

    python 3 support in setup.py

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
